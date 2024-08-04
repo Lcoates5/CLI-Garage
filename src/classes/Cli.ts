c06e94dc33a6ccb34cf9c76d7a5b6eb3b1403e72
Lauren Coates <lspassey@gmail.com>
2024-08-03 18:37:18 -0700

    uncommented index.ts for completed .ts files

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -301,6 +301,7 @@ class Cli {
         },
       ])
       .then((answers) => {
+        // TODO: check if the selected vehicle is the truck
         if (answers.vehicleToTow instanceof Truck) {
           console.log('A truck cannot tow itself');
           this.performActions();
@@ -315,7 +316,7 @@ class Cli {
           }
           this.performActions();
         }
-        // TODO: check if the selected vehicle is the truck
+        
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
         // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
       });
